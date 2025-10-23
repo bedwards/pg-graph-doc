84915e7a7c7ed1611d29b7467c3d93a14054b7fb
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-23 14:26:33 -0500

    allow mutations in graphql by marking graphql.resolve function as volatile instead of stable in the pg init script

--- pg-graph-doc/01_extensions.sql
+++ pg-graph-doc/01_extensions.sql
@@ -3,7 +3,7 @@ create extension if not exists documentdb_core;
 create extension if not exists documentdb cascade;
 
 create or replace function graphql(query text, variables jsonb default '{}'::jsonb)
-returns jsonb language sql stable as $$
+returns jsonb language sql volatile as $$
   select graphql.resolve(query, variables);
 $$;
 
