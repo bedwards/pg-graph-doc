d4f93164ff5932ad276cbe4219a746ee500f8a2d
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-23 07:08:49 -0500

    move command from docker-compose.yml to Dockerfile

--- run-sql.js
+++ run-sql.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import pg from "pg";
 
-dotenv.config({ path: '.env.example' });
+dotenv.config({ path: '.env.example', quiet: true });
 const { Client } = pg;
 const client = new Client({ connectionString: process.env.POSTGRES_URL });
 const sql = process.argv.slice(2).join(" ");
@@ -14,9 +14,3 @@ try {
 finally {
     await client.end();
 }
-
-// const searchPath = process.env.SEARCH_PATH || "app, public";
-
-// await client.query(`CREATE SCHEMA IF NOT EXISTS ${searchPath.split(",")[0].trim()};`);
-// await client.query(`SET search_path TO ${searchPath};`);
-
