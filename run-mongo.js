4f0503308146fbe4f6e70f5af6b9f9f3d0cf5f76
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-10-23 16:18:43 -0500

    support insert and other write operations in mongo script

--- run-mongo.js
+++ run-mongo.js
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
 dotenv.config({ path: ".env.example", quiet: true });
-const [collectionArg, queryArg = "{}", projArg = "{}"] = process.argv.slice(2);
+
+const [collectionArg, queryArg = "{}", projArg = "{}", insertArg] = process.argv.slice(2);
 
 if (!collectionArg) {
-  console.error("Usage: node run-mongo.js <collection> '<JSON query>' [projectionJSON]");
+  console.error("Usage: node run-mongo.js <collection> '<JSON query>' [projectionJSON] [insertJSON]");
   process.exit(1);
 }
 
@@ -13,17 +14,19 @@ const client = new MongoClient(process.env.MONGODB_URL);
 await client.connect();
 
 try {
-  const col = client.db().collection(collectionArg);
-  const query = JSON.parse(queryArg);
-  const projection = JSON.parse(projArg);
+  const col = client.db("postgres").collection(collectionArg);
 
-  if (Object.keys(query).length === 0) {
-    await col.createIndex({ created_at: -1 }).catch(e => raise(e));
+  if (insertArg) {
+    const doc = JSON.parse(insertArg);
+    const result = await col.insertOne(doc);
+    console.log("Inserted:", result.insertedId);
+  } else {
+    const query = JSON.parse(queryArg);
+    const projection = JSON.parse(projArg);
+    const cursor = col.find(query, { projection }).limit(50);
+    const docs = await cursor.toArray();
+    console.log(JSON.stringify(docs, null, 2));
   }
-
-  const cursor = col.find(query, { projection }).limit(50);
-  const docs = await cursor.toArray();
-  console.log(JSON.stringify(docs, null, 2));
 } finally {
   await client.close();
 }
